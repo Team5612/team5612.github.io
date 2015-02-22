ccbbe21f1ab56b4ddf0600891113ce514a61a876
Aurtik <txurtian@yahoo.com>
2015-02-21 22:27:58 -0500

    New Theme

---
+++ backup/js/snowkat.slick.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.single-item').slick({
+		autoplay: true,
+		autoplaySpeed: 5000,
+		mobileFirst: true,
+		arrows: false
+	});
+});
---
+++ js/wordpress.js
@@ -0,0 +1,3 @@
+function wordpressTheme() {
+	alert("Wordpress if you wish to speak with me about the theme please contact me at txurtian@yahoo.com - Txuritan");
+}
